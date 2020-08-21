77b2e5010dba19759c57a427267e8dcc5ddedb6f
JakubDryka <kuba.dryka@wp.pl>
2020-08-21 17:30:07 +0200

    modify and refactor Weather Forecast and add logout command

--- cogs/Commands.py
+++ cogs/Commands.py
@@ -14,19 +14,39 @@ class Commmands(commands.Cog):
         author = ctx.message.author.name
         await ctx.send(f'Hello {author}, you are koks')
 
+    @commands.command()
+    async def logout(self, ctx):
+        await ctx.message.add_reaction('✅')
+        print("Bot logged out successfully!")
+        await self.client.logout()
 
-    @commands.command() # TODO segregate commands into categories in command help_me#
+    @commands.command()
     async def help_me(self, ctx):
         await ctx.send("Hello I'm KuBot and here is list of  useful commands you may have want to use:")
         await ctx.send("If some commands or features don't work contact the creator.")
         await asyncio.sleep(4)
+        await ctx.send("CREATOR COMMANDS")
         await ctx.send("1. $load <name_of_extension> - loads extension to bot")
         await ctx.send("2. $unload <name_of_extension> - unloads extension to bot")
         await ctx.send("3. $reload <name_of_extension> - reloads extension to bot")
-        await ctx.send("4. $bots_latency - prints actual bot latency.")
-        await ctx.send("5. $clear_msg <amount> <onlybot> - deletes last X messages, add only_bot if you want to delete bots messages only.")
-        await ctx.send("6. $kick <member> <reason> - kicks that member, mind you need to tag them with '@'.")
-
+        await asyncio.sleep(1)
+        await ctx.send("WEATHER FORECAST COMMANDS")
+        await ctx.send("1. $weather_today <name of city> - prints actual wetaher in that city")
+        await ctx.send("2. $temperature_in <days> <name of city> - prints temperature in that city in X days from today")
+        await asyncio.sleep(1)
+        await ctx.send("GENERAL COMMANDS")
+        await ctx.send("1. $bots_latency - prints actual bot latency.")
+        await ctx.send("2. $hey - say hi to bot.")
+        await ctx.send("3. $logout - bot turns off.")
+        await ctx.send("4. $clear_msg <amount> <onlybot> - deletes last X messages, add only_bot if you want to delete bots messages only.")
+        await asyncio.sleep(1)
+        await ctx.send("MODERATION COMMANDS")
+        await ctx.send("1. $kick <member> <reason> - kicks that member, mind you need to tag them with '@'.")
+        await asyncio.sleep(1)
+        # await ctx.send("9. $bots_latency - prints actual bot latency.")
+        # await ctx.send("9. $bots_latency - prints actual bot latency.")
+        # await ctx.send("9. $bots_latency - prints actual bot latency.")
+        # await ctx.send("9. $bots_latency - prints actual bot latency.")
 
 
     @commands.command()
--- cogs/WeatherForecast.py
+++ cogs/WeatherForecast.py
@@ -1,7 +1,10 @@
-import discord
+import asyncio
 import requests
+import datetime
 from bs4 import BeautifulSoup
 from discord.ext import commands
+
+time_now = datetime.datetime.now()
 
 
 class WeatherForecast(commands.Cog):
@@ -14,30 +17,42 @@ class WeatherForecast(commands.Cog):
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36"}
         page = requests.get(url, headers=headers)
         parsed_page = BeautifulSoup(page.content, 'html.parser')
-        temperature = parsed_page.findAll(class_='potemp')
-        return temperature
+        temperatures = parsed_page.findAll(class_='potemp')
+        current_id = 'tp' + str(time_now.year).zfill(4) + str(time_now.month).zfill(2) + str(time_now.day).zfill(2)
+        description_today = parsed_page.find(id=current_id)
+        weather_data = [temperatures, description_today]
+        return weather_data
 
     @commands.command()
     async def weather_today(self, ctx, *, city):
-        city = city.replace(' ', '-')
-        temp = self.weather_scrapper(city)
-        city = city.replace('-', ' ')
         await ctx.send(f'Searching for weather in {city.capitalize()}...')
+        await asyncio.sleep(2)
+        city = city.replace(' ', '-')
+        data = self.weather_scrapper(city)
+        data = data[1].text.replace('2020', '2020.\n\n')
         try:
-            await ctx.send(f'Temperature is equal to {temp[0].text}')
-        except Exception:
+            await ctx.send(f'{data}')
+        except Exception as e:
             await ctx.send("You probably spelled that wrong")
+            print(e)
 
     @commands.command()
-    async def weather_tommorow(self, ctx, *, city):
+    async def temperature_in(self, ctx, days: int, *, city):
+        if 0 > days or days > 4:
+            await ctx.send(f'Wrong number of days, need to be between 0 and 4')
+            return
+        await ctx.send(f'Searching for temperature in {city.capitalize()}...')
+        await asyncio.sleep(2)
         city = city.replace(' ', '-')
-        temp = self.weather_scrapper(city)
-        city = city.replace('-', ' ')
-        await ctx.send(f'Searching for weather in {city.capitalize()}...')
+        weather_data = self.weather_scrapper(city)
         try:
-            await ctx.send(f'Tommorow temperature will be equal to {temp[1].text}')
-        except Exception:
-            await ctx.send("You probably spelled that wrong!")
+            temp = weather_data[0][days].text
+            new_time = time_now + datetime.timedelta(days=days)
+            await ctx.send(f'On day {new_time.day} of {new_time.strftime("%B")} {new_time.year} will be equal to {temp}')
+        except Exception as e:
+            await ctx.send(
+                "You probably spelled that wrong! Remember to write it in lower case and without polish signs!")
+            print(e)
 
 
 def setup(client):
